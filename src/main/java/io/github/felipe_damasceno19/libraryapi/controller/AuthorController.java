8ebaf4c894afc702e82d7cc57d15044ce74ddf95
Felipe Levi <felipegpt19@gmail.com>
2026-03-13 08:04:20 -0300

    feat(author-controller): add POST endpoint to save author

--- src/main/java/io/github/felipe_damasceno19/libraryapi/controller/AuthorController.java
+++ src/main/java/io/github/felipe_damasceno19/libraryapi/controller/AuthorController.java
@@ -1,20 +1,42 @@
 package io.github.felipe_damasceno19.libraryapi.controller;
 
 import io.github.felipe_damasceno19.libraryapi.controller.dto.AuthorDTO;
+import io.github.felipe_damasceno19.libraryapi.model.Author;
+import io.github.felipe_damasceno19.libraryapi.service.AuthorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/authors")
 public class AuthorController {
 
-    @PostMapping
-    public ResponseEntity save(@RequestBody AuthorDTO author){
-        return new ResponseEntity("Sucess! Author saved. "+ author, HttpStatus.CREATED);
+    private final AuthorService service;
+
+    public AuthorController(AuthorService service){
+        this.service = service;
     }
+
+    @PostMapping
+    public ResponseEntity<Void> save(@RequestBody AuthorDTO author){
+        Author authorEntity = author.mappinngAuthor();
+        service.save(authorEntity);
+
+        // Pega os dados da requisição atual e constroi uma nova url, pegando o Id e colocando no url
+        // localhost:8080/authors/id
+        URI location = ServletUriComponentsBuilder
+                        .fromCurrentRequest()
+                        .path("/{id}")
+                        .buildAndExpand(authorEntity.getId())
+                        .toUri();
+
+        //Retorna o status de created e o URI de location que criamos
+        return ResponseEntity.created(location).build();
+    }
+
+
 
 }
--- src/main/java/io/github/felipe_damasceno19/libraryapi/controller/dto/AuthorDTO.java
+++ src/main/java/io/github/felipe_damasceno19/libraryapi/controller/dto/AuthorDTO.java
@@ -1,8 +1,19 @@
 package io.github.felipe_damasceno19.libraryapi.controller.dto;
 
+import io.github.felipe_damasceno19.libraryapi.model.Author;
+
 import java.time.LocalDate;
 
 public record AuthorDTO(String name,
                         LocalDate birthDate,
-                        String nationality)
-{}
+                        String nationality) {
+
+    public Author mappinngAuthor(){
+        Author author = new Author();
+        author.setName(this.name);
+        author.setBirthDate(this.birthDate);
+        author.setNationality(this.nationality);
+        return author;
+    }
+
+}
