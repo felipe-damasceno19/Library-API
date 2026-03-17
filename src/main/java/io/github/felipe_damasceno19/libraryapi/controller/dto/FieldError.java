d3936f49877993bdefdd35083c08b010a141bc43
Felipe Levi <felipegpt19@gmail.com>
2026-03-16 21:45:58 -0300

    feat(api): add Error Response DTOs

---
+++ src/main/java/io/github/felipe_damasceno19/libraryapi/controller/dto/FieldError.java
@@ -0,0 +1,4 @@
+package io.github.felipe_damasceno19.libraryapi.controller.dto;
+
+public record FieldError(String field, String message) {
+}
---
+++ src/main/java/io/github/felipe_damasceno19/libraryapi/controller/dto/ResponseError.java
@@ -0,0 +1,16 @@
+package io.github.felipe_damasceno19.libraryapi.controller.dto;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+public record ResponseError(int status, String message, List<FieldError> errors) {
+
+    public static ResponseError defaultResponse(String message){
+        return new ResponseError(HttpStatus.BAD_REQUEST.value(), message, List.of());
+    }
+
+    public static ResponseError conflict(String message){
+        return new ResponseError(HttpStatus.CONFLICT.value(), message, List.of());
+    }
+}
