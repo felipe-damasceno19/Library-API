0cf58196f3c155a9bed931739cb01ca1caaf0a6f
Felipe Levi <felipegpt19@gmail.com>
2026-03-20 09:33:23 -0300

    feat(BookController): POST endpoint to register Book created

--- src/main/java/io/github/felipe_damasceno19/libraryapi/controller/BookController.java
+++ src/main/java/io/github/felipe_damasceno19/libraryapi/controller/BookController.java
@@ -1,14 +1,34 @@
 package io.github.felipe_damasceno19.libraryapi.controller;
 
+import io.github.felipe_damasceno19.libraryapi.controller.dto.RegisterBookDTO;
+import io.github.felipe_damasceno19.libraryapi.controller.dto.ResponseError;
+import io.github.felipe_damasceno19.libraryapi.exceptions.DuplicateRegistrationException;
 import io.github.felipe_damasceno19.libraryapi.service.BookService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/books")
+@RequestMapping("books")
 @RequiredArgsConstructor
 public class BookController {
 
     private final BookService service;
+
+    @PostMapping
+    public ResponseEntity<Object> save(@RequestBody @Valid RegisterBookDTO dto){
+        try{
+            return ResponseEntity.ok(dto);
+
+        }
+        catch (DuplicateRegistrationException e){
+            var dtoError = ResponseError.conflict(e.getMessage());
+            return ResponseEntity.status(dtoError.status()).body(dtoError);
+        }
+
+    }
 }
--- src/main/java/io/github/felipe_damasceno19/libraryapi/controller/dto/RegisterBookDTO.java
+++ src/main/java/io/github/felipe_damasceno19/libraryapi/controller/dto/RegisterBookDTO.java
@@ -1,15 +1,35 @@
 package io.github.felipe_damasceno19.libraryapi.controller.dto;
 
+import io.github.felipe_damasceno19.libraryapi.model.Book;
 import io.github.felipe_damasceno19.libraryapi.model.BookGenre;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record RegisterBookDTO(String isbn,
-          String name,
-          LocalDate publicationDate,gi
-          BookGenre genre,
-          BigDecimal price,
-          UUID authorId) {
+public record RegisterBookDTO(
+        @NotBlank(message = "Required field")
+        @Size(min = 10, max = 14, message = "Field out of bounds!")
+        String isbn,
+        @NotBlank(message = "Required field")
+        @Size(min = 2, max = 100, message = "Field out of bounds!")
+        String name,
+        @Past(message = "Cannot be a future date")
+        LocalDate publicationDate,
+        BookGenre genre,
+        BigDecimal price,
+        @NotNull(message = "Required field")
+        UUID authorId) {
+
+    void mappingBook(){
+        Book book = new Book();
+        book.setIsbn(this.isbn);
+        book.setName(this.name);
+        book.setGenre(this.genre);
+        book.setPrice(this.price);
+    }
 }
